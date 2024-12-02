b8615fbd6101052d64ec891426195d5fd341f592
Sampurna <sampurng@gmail.com>
2024-12-02 17:10:01 -0500

    assignment-09

--- source/index.js
+++ source/index.js
@@ -1,10 +1,29 @@
 module.exports.handler = async (event) => {
+  // console.log("EHEHEHEHhererererererererererere", process.env.SECRET_NAME);
+
   console.log(event.Records[0].Sns, event);
   const axios = require("axios");
+  const AWS = require("aws-sdk");
+  const secretsManager = new AWS.SecretsManager();
+  async function getMailgunCredentials() {
+    try {
+      const secretValue = await secretsManager
+        .getSecretValue({ SecretId: process.env.SECRET_NAME })
+        .promise();
+      return JSON.parse(secretValue.SecretString);
+    } catch (error) {
+      console.error("Error fetching secrets:", error);
+      throw error;
+    }
+  }
+
+  console.log("Here", process.env.SECRET_NAME);
   //   console.log("HEHEH", process.env.DB_CONNECTION_URL, process.env.API_KEY);
   try {
     // Send POST request
-    const DOMAIN = process.env.DOMAIN;
+    const secrets = await getMailgunCredentials();
+    console.log(secrets, "SECRETSSSS");
+    const DOMAIN = secrets.DOMAIN;
     const message = JSON.parse(event.Records[0].Sns.Message);
     const response = await axios.post(
       `https://api.mailgun.net/v3/${DOMAIN}.sampurna.xyz/messages`,
@@ -18,7 +37,7 @@ module.exports.handler = async (event) => {
       {
         auth: {
           username: "api",
-          password: process.env.API_KEY,
+          password: secrets.API_KEY,
         },
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
